e36e6c228053b2b9957c27fe322806c25b9ab56b
Hirbod <77854729+hirbodprime@users.noreply.github.com>
2022-05-31 21:19:31 +0430

    first commit

---
+++ oldwolves.py
@@ -0,0 +1,197 @@
+from pyrogram import Client, filters
+from time import sleep
+bot = Client("old", 1723719, 'dce2a0e1a8376fec526b76bb99746238')
+
+
+
+
+# bot_id = 1344976082
+# bot_hash = 'AAEEz5LmTQ1docDXrEbYcwnnH3Ycogq57gE'
+# bot = Client('helper', bot_id, bot_hash, bot_token="1344976082:AAEEz5LmTQ1docDXrEbYcwnnH3Ycogq57gE")
+# bots = {}
+
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("ساخته شده"))
+def kubs(c, m):
+    # print(m)
+    # print(m.reply_markup)
+    # print(m.reply_markup.inline_keyboard[0][0])
+    url = m.reply_markup.inline_keyboard[0][0].url
+    sleep(1)
+    bot.send_message("werewolfbot", f"/start {url[31:]}")
+
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("بازیکن های زنده: 5/5"))
+def koft(c, m): m.reply_text("/up")
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("بازیکن های زنده: 6/6"))
+def koft(c, m): m.reply_text("/up")
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("بازیکن های زنده: 7/7"))
+def koft(c, m): m.reply_text("/up")
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("بازیکن های زنده: 8/8"))
+def koft(c, m): m.reply_text("/up")
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("بازیکن های زنده: 9/9"))
+def koft(c, m): m.reply_text("/up")
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("بازیکن های زنده: 10/10"))
+def koft(c, m): m.reply_text("/up")
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("بازیکن های زنده: 11/11"))
+def koft(c, m): m.reply_text("/up")
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("بازیکن های زنده: 12/12"))
+def koft(c, m): m.reply_text("/up")
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("بازیکن های زنده: 13/13"))
+def koft(c, m): m.reply_text("/up")
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("بازیکن های زنده: 14/14"))
+def koft(c, m): m.reply_text("/up")
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("بازیکن های زنده: 15/15"))
+def koft(c, m): m.reply_text("/up")
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("بازیکن های زنده: 16/16"))
+def koft(c, m): m.reply_text("/up")
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("بازیکن های زنده: 17/17"))
+def koft(c, m): m.reply_text("/up")
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("بازیکن های زنده: 18/18"))
+def koft(c, m): m.reply_text("/up")
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("بازیکن های زنده: 19/19"))
+def koft(c, m): m.reply_text("/up")
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("بازیکن های زنده: 20/20"))
+def koft(c, m): m.reply_text("/up")
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("بازیکن های زنده: 21/21"))
+def koft(c, m): m.reply_text("/up")
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("بازیکن های زنده: 22/22"))
+def koft(c, m): m.reply_text("/up")
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("بازیکن های زنده: 23/23"))
+def koft(c, m): m.reply_text("/up")
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("بازیکن های زنده: 24/24"))
+def koft(c, m): m.reply_text("/up")
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("بازیکن های زنده: 25/25"))
+def koft(c, m): m.reply_text("/up")
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("بازیکن های زنده: 26/26"))
+def koft(c, m): m.reply_text("/up")
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("بازیکن های زنده: 27/27"))
+def koft(c, m): m.reply_text("/up")
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("بازیکن های زنده: 28/28"))
+def koft(c, m): m.reply_text("/up")
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("بازیکن های زنده: 29/29"))
+def koft(c, m): m.reply_text("/up")
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("بازیکن های زنده: 30/30"))
+def koft(c, m): m.reply_text("/up")
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("بازیکن های زنده: 31/31"))
+def koft(c, m): m.reply_text("/up")
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("بازیکن های زنده: 32/32"))
+def koft(c, m): m.reply_text("/up")
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("بازیکن های زنده: 33/33"))
+def koft(c, m): m.reply_text("/up")
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("بازیکن های زنده: 34/34"))
+def koft(c, m): m.reply_text("/up")
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("بازیکن های زنده: 35/35"))
+def koft(c, m): m.reply_text("/up")
+
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("لیست اپدیت شد"))
+def koft(c, m): m.reply_text("/saveyourrole@role_ww_bot", quote = False)
+
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("مرده -")& ~filters.regex("مدت زمان بازی:")& ~filters.regex("بازیکنان مرده:"))
+def jt8(c, m): m.reply_text("/up")
+# @bot.on_message(filters.chat(-1001161760530) & filters.regex("خب روز شد."))
+# def jt7(c, m): m.reply_text("موفق باشید", quote=False )
+
+# @bot.on_message(filters.chat(-1001302566490) & filters.regex("مدت زمان بازی:"))
+# def jt7(c, m): m.reply_text("/confirm@TsWwPlus_Bot")
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("سازنده گروه:"))
+def koft(c, m): m.reply_text("/d")
+
+@bot.on_message(filters.chat(-1001161760530) & filters.regex( "#شکار" )& ~filters.user(['@executrixbot','@partnerwolf_bot','@partnerwolf_robot' ]))
+def koft(c, m):
+    m.reply_text("/pin")
+    sleep(3)
+    m.reply_text("/shekar@role_ww_bot")
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("#shekarm" )& ~filters.user(['@executrixbot','@partnerwolf_bot','@partnerwolf_robot']))
+def koft(c, m):
+    m.reply_text("/pin")
+    sleep(3)
+    m.reply_text("/shekar@role_ww_bot")
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("چقدر کمین! من با این تعداد بازیکن بازی رو شروع نمیکنم 😢"))
+def koft(c, m): m.reply_text("/admin")
+@bot.on_message(filters.chat(-1001161760530) & filters.regex( "#شکارچی" ) & ~filters.user(['@executrixbot','@partnerwolf_bot' ,'@partnerwolf_robot']))
+def koft(c, m):
+    m.reply_text("/pin")
+    sleep(3)
+    m.reply_text("/shekar@role_ww_bot")
+
+    @bot.on_message(filters.chat(-1001161760530) & filters.regex( "#شکارم") & ~filters.user(['@executrixbot','@partnerwolf_bot','@partnerwolf_robot' ]))
+    def koft(c, m):
+        m.reply_text("/pin")
+        sleep(3)
+        m.reply_text("/shekar@role_ww_bot")
+
+    @bot.on_message(filters.chat(-1001161760530) & filters.regex("ch")& ~filters.user(['@executrixbot','@partnerwolf_bot','@partnerwolf_robot']))
+    def koft(c, m):
+        m.reply_text("/pin")
+        sleep(3)
+        m.reply_text("/shekar@role_ww_bot")
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("#players") & ~filters.edited)
+def jt4(c, m):
+    # m.reply_text("/pin")
+    # sleep(3)
+    m.reply_text("/extend@werewolfbot 300", quote=False)
+    sleep(5)
+    m.reply_text("/flee", quote=False)
+    sleep(2)
+    m.reply_text("/replytag 500", quote = False)
+    # sleep(2)
+    # m.reply_text("/fillit@TsWwPlus_Bot")
+    # m.reply_text("/tag 0 100", quote = False)
+    # sleep(5)
+    # m.reply_text("/mtag 0 100 join bmola", quote = False)
+
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("ایول بازی شروع شد 😃 یه کم صبر کنین نقشاتونو بهتون بگم")& ~filters.edited)
+def jt5(c, m):
+    m.reply_text("#next",quote=False )
+    sleep(2)
+    m.reply_text('/deltgs@partnerwolf_robot', quote=False)
+    m.reply_text('/deltag@werewolf_Tagger_bot', quote=False)
+    m.reply_text('/del_tags', quote=False)
+
+# sleep(2)
+# @bot.on_message(filters.chat(-1001161760530) & filters.regex("#next"))
+# def jt5(c, m):m.reply_text('/pin')
+# @bot.on_message(filters.chat(-1001161760530) & filters.regex("GoodLuck!")& ~filters.edited)
+# def jt5(c, m): m.reply_text("/startchaos@werewolfbot", quote = False )
+# sleep(2)
+
+
+
+@bot.on_message(filters.chat(-1001161760530) & filters.regex("فقط 10  ثانیه دیگه وقت دارید که وارد بازی شید...")& ~filters.edited)
+def jt(c, m):
+    m.reply_text("/stop", quote = False)
+    sleep(3)
+
+# @bot.on_message(filters.chat(-1001161760530) & filters.regex("tag kon mashti"))
+# def jt(c, m): m.reply_text("Tag", quote = False)
+# sleep(3)
+#
+# @bot.on_message(filters.chat(-1001161760530) & filters.regex("Stop"))
+# def jt(c, m): m.reply_text("OK", quote = False)
+#
+# sleep(3)
+# @bot.on_message(filters.chat(-1001161760530) & filters.regex("OK"))
+# def jt(c, m): m.reply_text("stop", quote = False)
+# sleep(3)
+#
+# @bot.on_message(filters.command("Tag", None) & filters.me)
+# def tag(c, m):
+#     global tag_on
+#     tag_on = True
+#
+#     for user in bot.iter_chat_members(m.chat.id, int(m.command[1]) if len(m.command) >= 2 else None):
+#         if user.user.is_bot == False and user.user.is_deleted == False:
+#             if tag_on:
+#                 bot.send_message(m.chat.id, f"{user.user.mention} Next")
+#             else:
+#                 exit()
+#
+#
+# @bot.on_message(filters.command("stop", None) & filters.me)
+# def stoptg(c, m):
+#     global tag_on
+#     tag_on = False
+#     m.reply("stoped!")
+#
+
+
+bot.run()
